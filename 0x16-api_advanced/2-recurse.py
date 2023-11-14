4b2662b2e8d62d67fb16d6705006babfabaa0d48
Reliccode <bryanmaritim@gmail.com>
2023-11-14 09:18:46 +0300

    fix: Listing comprehension directly in the recurse func removing add_title

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,46 +1,66 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
+Queries the Reddit API and retrieves the titles of the top hot posts.
 """
+
 import requests
-import sys
 
 
-def add_title(hot_list, hot_posts):
-    """ Adds item into a list """
-    if len(hot_posts) == 0:
-        return
-    hot_list.append(hot_posts[0]['data']['title'])
-    hot_posts.pop(0)
-    add_title(hot_list, hot_posts)
+def extract_titles(hot_posts):
+    """Extract titles from a list of posts."""
+    return [post['data']['title'] for post in hot_posts]
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': u_agent
-    }
+def recurse(subreddit, hot_list=None, after=None):
+    """
+    Recursively queries the Reddit API to retrieve titles of hot posts.
 
-    params = {
-        'after': after
-    }
+    Args:
+        subreddit (str): The subreddit to query.
+        hot_list (list, optional): List to store titles.
+        after (str, optional): Parameter for pagination.
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
+    Returns:
+        list or None: List of titles if successful, None if there's an issue.
+    """
+    if hot_list is None:
+        hot_list = []
 
-    if res.status_code != 200:
-        print(f"Error: Unable to fetch data from Reddit API. Status Code: {res.status_code}")
+    user_agent = 'Mozilla/5.0'
+    headers = {'User-Agent': user_agent}
+    params = {'after': after} if after else {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch data from Reddit API. Status Code:
+              {response.status_code}")
         return None
 
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    hot_list.extend([post['data']['title'] for post in hot_posts])
-    after = dic['data']['after']
+    data = response.json().get('data', {})
+    hot_posts = data.get('children', [])
+
+    hot_list.extend(extract_titles(hot_posts))
+
+    after = data.get('after')
     if not after:
         return hot_list
+
     return recurse(subreddit, hot_list=hot_list, after=after)
+
+
+# Example usage:
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit_name = sys.argv[1]
+        result = recurse(subreddit_name)
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
